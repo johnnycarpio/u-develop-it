8afd667f18fddbd147f894cbaa3ba5c05a38ba5e
Johnny Carpio <johnnycarpio04@gmail.com>
2022-03-11 10:11:39 +0800

    added voters table

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: '',
+  password: 'Jani@1236363',
   database: 'election'
 });
 
